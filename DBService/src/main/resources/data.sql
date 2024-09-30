3d9289688c530d86f3acf4dc89c0fd7d00522fed
suraj gupta <surajgupta@idrive.com>
2024-09-30 20:44:47 +0530

    add files

---
+++ DBService/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT  INTO persons(name) VALUES('Surya');
+INSERT  INTO persons(name) VALUES('Seth');
---
+++ DBService/src/main/resources/insertEmployee.sql
@@ -0,0 +1,2 @@
+INSERT  INTO employee(name) VALUES('Lucky');
+INSERT  INTO employee(name) VALUES('Gupta');
